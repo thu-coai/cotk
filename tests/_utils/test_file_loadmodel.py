666ad4c5d97ff76e4e3666b69cf3cef0b564eeee
Yilin Niu <niuyl14@mails.tsinghua.edu.cn>
2019-07-04 13:38:14 +0800

    Add more test for file_utils.py (#177)

--- tests/_utils/test_file_loadmodel.py
+++ tests/_utils/test_file_loadmodel.py
@@ -17,6 +17,10 @@ class TestFileUtils():
 		data_dir = './tests/_utils/data'
 		res_path = load_model_from_url('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
 
+		with pytest.raises(ValueError) as excinfo:
+			load_model_from_url('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
+		assert("model existed. If you want to delete the existing model." in str(excinfo.value))
+
 		assert(res_path == os.path.join(cache_dir, 'models', 'test.zip'))
 		assert(_get_file_sha256(res_path) == _get_file_sha256(os.path.join(data_dir, 'test.zip')))
 
--- tests/_utils/test_file_utils.py
+++ tests/_utils/test_file_utils.py
@@ -20,6 +20,19 @@ class TestFileUtils():
 
 		cache_dir = './tests/_utils/dataset_cache'
 		config_dir = './tests/_utils/dummy_coai'
+
+		with pytest.raises(FileNotFoundError) as excinfo:
+			get_resource_file_path('resources://coai', cache_dir=cache_dir, config_dir='wrongpath')
+		assert("not found" in str(excinfo.value))
+
+		with pytest.raises(ValueError) as excinfo:
+			get_resource_file_path('resources://coai#wrongtype', cache_dir=cache_dir, config_dir=config_dir)
+		assert("differs with res_type" in str(excinfo.value))
+
+		with pytest.raises(ValueError) as excinfo:
+			get_resource_file_path('resources://coai@wronglink', cache_dir=cache_dir, config_dir=config_dir)
+		assert("source wronglink wrong" in str(excinfo.value))
+
 		res_path = get_resource_file_path('resources://coai', cache_dir=cache_dir, config_dir=config_dir)
 		
 		assert(res_path == os.path.join(cache_dir, '146ce545f2ed0a8767aadae8f2921f7951df817b39b8f7d0db48bce87e3eaf69'))
@@ -60,6 +73,7 @@ class TestFileUtils():
 		cache_dir = './tests/_utils/dataset_cache'
 		config_dir = './tests/_utils/dummy_coai'
 		res_path = get_resource_file_path('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
+		res_path = get_resource_file_path('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
 
 		assert(res_path == os.path.join(cache_dir, 'f1043836933af4b8b28973d259c0c77f5049de2dff8d0d1f305c65f3c497b3b1'))
 		assert(os.path.exists(res_path))
@@ -76,7 +90,21 @@ class TestFileUtils():
 		cache_dir = './tests/_utils/dataset_cache'
 		config_dir = './tests/_utils/dummy_coai'
 		data_dir = './tests/_utils/data'
+
+		with pytest.raises(ValueError) as excinfo:
+			import_local_resources('test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir)
+		assert("file_id must startswith" in str(excinfo.value))
+
+		with pytest.raises(ValueError) as excinfo:
+			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'mscoco.zip'), cache_dir=cache_dir, config_dir=config_dir)
+		assert("bad hashtag" in str(excinfo.value))
+
 		res_path = import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir)
+
+		with pytest.raises(ValueError) as excinfo:
+			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir, ignore_exist_error=True)
+			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir, ignore_exist_error=False)		
+		assert("resources existed. If you want to delete the existing resources." in str(excinfo.value))
 
 		assert(res_path == os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'))
 		assert(os.path.exists(res_path))
