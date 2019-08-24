a7f98bc40fa40325f3994692532c54d3cc88d8f9
aaa123git <43716234+aaa123git@users.noreply.github.com>
2019-08-24 12:43:53 +0800

    fix file processor on windows (#318)
    * remove unnecessary parens after 'assert' keyword
    
    * use pathlib.Path to solve file path problems

--- tests/_utils/test_file_processor.py
+++ tests/_utils/test_file_processor.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import requests_mock
 import os
+import pathlib
 import hashlib
 import json
 import shutil
@@ -17,76 +18,76 @@ def r_mock():
 def check(file1, file2):
 	with open(file1, 'r', encoding='utf-8') as f1:
 		with open(file2, 'r', encoding='utf-8') as f2:
-			assert(f1.read() == f2.read())
+			assert f1.read() == f2.read()
 
 class TestFileUtils():
 	def test_MSCOCO_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
-		res_path = get_resource_file_path('./tests/_utils/data/mscoco.zip#MSCOCO')
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
+		res_path = get_resource_file_path(str(pathlib.Path('./tests/_utils/data/mscoco.zip#MSCOCO')))
 
 		filenames = os.listdir(res_path)
-		assert(res_path == './tests/_utils/data/mscoco.zip_unzip/mscoco')
-		assert(sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'mscoco'))))
+		assert res_path == str(pathlib.Path('./tests/_utils/data/mscoco.zip_unzip/mscoco'))
+		assert sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'mscoco')))
 		for filename in filenames:
 			check(os.path.join(res_path, filename), os.path.join(data_dir, 'mscoco', filename))
 
-		shutil.rmtree('./tests/_utils/data/mscoco.zip_unzip')
+		shutil.rmtree(str(pathlib.Path('./tests/_utils/data/mscoco.zip_unzip')))
 
 	def test_OpenSubtitles_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
-		res_path = get_resource_file_path('./tests/_utils/data/opensubtitles.zip#OpenSubtitles')
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
+		res_path = get_resource_file_path(str(pathlib.Path('./tests/_utils/data/opensubtitles.zip#OpenSubtitles')))
 
 		filenames = os.listdir(res_path)
-		assert(res_path == './tests/_utils/data/opensubtitles.zip_unzip/opensubtitles')
-		assert(sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'opensubtitles'))))
+		assert res_path == str(pathlib.Path('./tests/_utils/data/opensubtitles.zip_unzip/opensubtitles'))
+		assert sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'opensubtitles')))
 		for filename in filenames:
 			check(os.path.join(res_path, filename), os.path.join(data_dir, 'opensubtitles', filename))
 
-		shutil.rmtree('./tests/_utils/data/opensubtitles.zip_unzip')
+		shutil.rmtree(str(pathlib.Path('./tests/_utils/data/opensubtitles.zip_unzip')))
 
 	def test_Ubuntu_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
-		res_path = get_resource_file_path('./tests/_utils/data/ubuntu_dataset.zip#Ubuntu')
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
+		res_path = get_resource_file_path(str(pathlib.Path('./tests/_utils/data/ubuntu_dataset.zip#Ubuntu')))
 
 		filenames = os.listdir(res_path)
-		assert(res_path == './tests/_utils/data/ubuntu_dataset.zip_unzip/ubuntu_dataset')
-		assert(sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'ubuntu_dataset'))))
+		assert res_path == str(pathlib.Path('./tests/_utils/data/ubuntu_dataset.zip_unzip/ubuntu_dataset'))
+		assert sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'ubuntu_dataset')))
 		for filename in filenames:
 			check(os.path.join(res_path, filename), os.path.join(data_dir, 'ubuntu_dataset', filename))
 
-		shutil.rmtree('./tests/_utils/data/ubuntu_dataset.zip_unzip')
+		shutil.rmtree(str(pathlib.Path('./tests/_utils/data/ubuntu_dataset.zip_unzip')))
 
 	def test_SwitchboardCorpus_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
-		res_path = get_resource_file_path('./tests/_utils/data/switchboard_corpus.zip#SwitchboardCorpus')
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
+		res_path = get_resource_file_path(str(pathlib.Path('./tests/_utils/data/switchboard_corpus.zip#SwitchboardCorpus')))
 
 		filenames = os.listdir(res_path)
-		assert(res_path == './tests/_utils/data/switchboard_corpus.zip_unzip/switchboard_corpus')
-		assert(sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'switchboard_corpus'))))
+		assert res_path == str(pathlib.Path('./tests/_utils/data/switchboard_corpus.zip_unzip/switchboard_corpus'))
+		assert sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'switchboard_corpus')))
 		for filename in filenames:
 			check(os.path.join(res_path, filename), os.path.join(data_dir, 'switchboard_corpus', filename))
 
-		shutil.rmtree('./tests/_utils/data/switchboard_corpus.zip_unzip')
+		shutil.rmtree(str(pathlib.Path('./tests/_utils/data/switchboard_corpus.zip_unzip')))
 
 	def test_glove50d_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
-		res_path = get_resource_file_path('./tests/_utils/data/glove.6B.50d.zip#Glove50d')
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
+		res_path = get_resource_file_path(str(pathlib.Path('./tests/_utils/data/glove.6B.50d.zip#Glove50d')))
 
 		filenames = os.listdir(res_path)
-		assert(res_path == './tests/_utils/data/glove.6B.50d.zip_unzip/50d')
-		assert(sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'glove', '50d'))))
+		assert res_path == str(pathlib.Path('./tests/_utils/data/glove.6B.50d.zip_unzip/50d'))
+		assert sorted(filenames) == sorted(os.listdir(os.path.join(data_dir, 'glove', '50d')))
 		for filename in filenames:
 			check(os.path.join(res_path, filename), os.path.join(data_dir, 'glove', '50d', filename))
 
-		shutil.rmtree('./tests/_utils/data/glove.6B.50d.zip_unzip')
+		shutil.rmtree(str(pathlib.Path('./tests/_utils/data/glove.6B.50d.zip_unzip')))
 
--- tests/_utils/test_file_utils.py
+++ tests/_utils/test_file_utils.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import requests_mock
 import os
+import pathlib
 import hashlib
 import json
 import shutil
@@ -18,101 +19,101 @@ class TestFileUtils():
 	def test_get_resource(self, r_mock):
 		r_mock.get('http://coai.cs.tsinghua.edu.cn/', text='coai')
 
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
 
 		with pytest.raises(FileNotFoundError) as excinfo:
 			get_resource_file_path('resources://coai', cache_dir=cache_dir, config_dir='wrongpath')
-		assert("not found" in str(excinfo.value))
+		assert "not found" in str(excinfo.value)
 
 		with pytest.raises(ValueError) as excinfo:
 			get_resource_file_path('resources://coai#wrongtype', cache_dir=cache_dir, config_dir=config_dir)
-		assert("differs with res_type" in str(excinfo.value))
+		assert "differs with res_type" in str(excinfo.value)
 
 		with pytest.raises(ValueError) as excinfo:
 			get_resource_file_path('resources://coai@wronglink', cache_dir=cache_dir, config_dir=config_dir)
-		assert("source wronglink wrong" in str(excinfo.value))
+		assert "source wronglink wrong" in str(excinfo.value)
 
 		res_path = get_resource_file_path('resources://coai', cache_dir=cache_dir, config_dir=config_dir)
 		
-		assert(res_path == os.path.join(cache_dir, '146ce545f2ed0a8767aadae8f2921f7951df817b39b8f7d0db48bce87e3eaf69'))
-		assert(os.path.exists(res_path))
+		assert res_path == os.path.join(cache_dir, '146ce545f2ed0a8767aadae8f2921f7951df817b39b8f7d0db48bce87e3eaf69')
+		assert os.path.exists(res_path)
 
 		hash_sha256 = hashlib.sha256()
 		with open(res_path, "rb") as fin:
 			for chunk in iter(lambda: fin.read(4096), b""):
 				hash_sha256.update(chunk)
-		assert(hash_sha256.hexdigest() == "146ce545f2ed0a8767aadae8f2921f7951df817b39b8f7d0db48bce87e3eaf69")
+		assert hash_sha256.hexdigest() == "146ce545f2ed0a8767aadae8f2921f7951df817b39b8f7d0db48bce87e3eaf69"
 
 		meta_path = res_path + '.json'
-		assert(os.path.exists(meta_path))
+		assert os.path.exists(meta_path)
 		with open(meta_path, 'r', encoding='utf-8') as meta_file:
 			meta = json.load(meta_file)
-			assert(meta['local_path'] == res_path)
+			assert meta['local_path'] == res_path
 		
 		shutil.rmtree(cache_dir)
 
 	def test_download_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
 		res_path = get_resource_file_path('resources://test@amazon', cache_dir=cache_dir, config_dir=config_dir)
 		res_path = get_resource_file_path('resources://test', cache_dir=cache_dir, config_dir=config_dir)
 
-		assert(res_path == os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'))
-		assert(os.path.exists(res_path))
+		assert res_path == os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+		assert os.path.exists(res_path)
 
 		meta_path = os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08.json')
-		assert(os.path.exists(meta_path))
+		assert os.path.exists(meta_path)
 		with open(meta_path, 'r', encoding='utf-8') as meta_file:
 			meta = json.load(meta_file)
-			assert(meta['local_path'] == res_path)
+			assert meta['local_path'] == res_path
 		
 		shutil.rmtree(cache_dir)
 
 	def test_download_data(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
 		res_path = get_resource_file_path('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
 		res_path = get_resource_file_path('https://cotk-data.s3-ap-northeast-1.amazonaws.com/test.zip', cache_dir=cache_dir)
 
-		assert(res_path == os.path.join(cache_dir, 'f1043836933af4b8b28973d259c0c77f5049de2dff8d0d1f305c65f3c497b3b1'))
-		assert(os.path.exists(res_path))
+		assert res_path == os.path.join(cache_dir, 'f1043836933af4b8b28973d259c0c77f5049de2dff8d0d1f305c65f3c497b3b1')
+		assert os.path.exists(res_path)
 
 		meta_path = os.path.join(cache_dir, 'f1043836933af4b8b28973d259c0c77f5049de2dff8d0d1f305c65f3c497b3b1.json')
-		assert(os.path.exists(meta_path))
+		assert os.path.exists(meta_path)
 		with open(meta_path, 'r', encoding='utf-8') as meta_file:
 			meta = json.load(meta_file)
-			assert(meta['local_path'] == res_path)
+			assert meta['local_path'] == res_path
 		
 		shutil.rmtree(cache_dir)
 
 	def test_import_local_resource(self):
-		cache_dir = './tests/_utils/dataset_cache'
-		config_dir = './tests/_utils/dummy_coai'
-		data_dir = './tests/_utils/data'
+		cache_dir = str(pathlib.Path('./tests/_utils/dataset_cache'))
+		config_dir = str(pathlib.Path('./tests/_utils/dummy_coai'))
+		data_dir = str(pathlib.Path('./tests/_utils/data'))
 
 		with pytest.raises(ValueError) as excinfo:
 			import_local_resources('test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir)
-		assert("file_id must startswith" in str(excinfo.value))
+		assert "file_id must startswith" in str(excinfo.value)
 
 		with pytest.raises(ValueError) as excinfo:
 			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'mscoco.zip'), cache_dir=cache_dir, config_dir=config_dir)
-		assert("bad hashtag" in str(excinfo.value))
+		assert "bad hashtag" in str(excinfo.value)
 
 		res_path = import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir)
 
 		with pytest.raises(ValueError) as excinfo:
 			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir, ignore_exist_error=True)
 			import_local_resources('resources://test', local_path=os.path.join(data_dir, 'test.zip'), cache_dir=cache_dir, config_dir=config_dir, ignore_exist_error=False)		
-		assert("resources existed. If you want to delete the existing resources." in str(excinfo.value))
+		assert "resources existed. If you want to delete the existing resources." in str(excinfo.value)
 
-		assert(res_path == os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'))
-		assert(os.path.exists(res_path))
+		assert res_path == os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+		assert os.path.exists(res_path)
 
 		meta_path = os.path.join(cache_dir, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08.json')
-		assert(os.path.exists(meta_path))
+		assert os.path.exists(meta_path)
 		with open(meta_path, 'r', encoding='utf-8') as meta_file:
 			meta = json.load(meta_file)
-			assert(meta['local_path'] == res_path)
+			assert meta['local_path'] == res_path
 		
 		shutil.rmtree(cache_dir)
