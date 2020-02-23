e6f26626762985064c5bf7472f97e3c214ebb63b
HUANG Fei <hzhwcmhf@gmail.com>
2020-02-23 16:12:07 +0800

    Update single_turn_dialog.py

--- cotk/dataloader/single_turn_dialog.py
+++ cotk/dataloader/single_turn_dialog.py
@@ -78,12 +78,12 @@ class SingleTurnDialog(LanguageProcessing):
 		elif pretrained == "gpt2":
 			if not isinstance(tokenizer, PretrainedTokenizer):
 				raise ValueError("tokenize should be loaded first if you want a gpt2 dataloader")
-			vocab = PretrainedVocab(tokenizer)
+			vocab = PretrainedVocab(tokenizer.tokenizer)
 			with FieldContext.set_parameters(tokenizer=tokenizer,\
 					vocab=vocab, \
 					max_sent_length=max_sent_length, \
 					convert_to_lower_letter=convert_to_lower_letter):
-				super().__init__(file_id, [("post", "SentenceGPT2"), ("resp", "SentenceGPT2")])
+				super().__init__(file_id, OrderedDict([("post", "SentenceGPT2"), ("resp", "SentenceGPT2")]))
 			self.set_default_field("train", "post")
 
 		else:
