fdd69727ce810e5b6fbfb1465ae27ddaf0768d84
HUANG Fei <hzhwcmhf@gmail.com>
2020-03-10 16:39:18 +0800

    fix test

--- tests/dataloader/test_tokenizer.py
+++ tests/dataloader/test_tokenizer.py
@@ -7,10 +7,7 @@ import pytest
 from pytest_mock import mocker
 import numpy as np
 
-from transformers.tokenization_gpt2 import GPT2Tokenizer
-
-from cotk.dataloader.tokenizer import Tokenizer, SimpleTokenizer, PretrainedTokenizer
-
+from cotk.dataloader import Tokenizer, SimpleTokenizer, PretrainedTokenizer
 
 def setup_module():
 	import random
@@ -18,7 +15,7 @@ def setup_module():
 	import numpy as np
 	np.random.seed(0)
 
-class TestTokernizer():
+class TestTokenizer():
 	def base_test_tokenize_sentences(self, toker: Tokenizer, sentences: List[str]):
 		tokenized_sentences = toker.tokenize_sentences(sentences)
 		assert isinstance(tokenized_sentences, list)
@@ -27,7 +24,7 @@ class TestTokernizer():
 			assert isinstance(sent, list)
 			for token in sent:
 				assert isinstance(token, str)
-		
+
 	def base_test_tokenize_sessions(self, toker: Tokenizer, sessions: List[List[str]]):
 		tokenized_sessions = toker.tokenize_sessions(sessions)
 		assert isinstance(tokenized_sessions, list)
@@ -49,8 +46,8 @@ def load_SimpleTokenizer():
 		return SimpleTokenizer(method=method, special_tokens=special_tokens)
 	return _load_SimpleTokenizer
 
-class TestSimpleTokenizer(TestTokernizer):
-	
+class TestSimpleTokenizer(TestTokenizer):
+
 	@pytest.mark.dependency()
 	def test_init(self, load_SimpleTokenizer):
 		sentences = [
@@ -63,7 +60,7 @@ class TestSimpleTokenizer(TestTokernizer):
 		]
 		super().base_test_tokenize_sentences(load_SimpleTokenizer('space'), sentences)
 		super().base_test_tokenize_sessions(load_SimpleTokenizer('nltk'), sessions)
-	
+
 	@pytest.mark.dependency(depends=["TestSimpleTokenizer::test_init"])
 	def test_convert_tokens_to_sentence(self, load_SimpleTokenizer):
 		tokens = ['i', '\'ve', 'been', 'to', 'John', '\'s', 'home', '!']
@@ -73,7 +70,7 @@ class TestSimpleTokenizer(TestTokernizer):
 		space = load_SimpleTokenizer('space')
 		space_res = ' '.join(tokens)
 		assert space.convert_tokens_to_sentence(tokens) == space_res
-	
+
 	@pytest.mark.dependency(depends=["TestSimpleTokenizer::test_init"])
 	def test_get_setting_hash(self, load_SimpleTokenizer):
 		toker1 = load_SimpleTokenizer('space', special_tokens=['<pad>', '<unk>', '<go>', '<eos>'])
@@ -92,12 +89,13 @@ def load_PretrainedTokenizer():
 	def _load_PretrainedTokenizer(unk_token='<|endoftext|>'):
 		vocab_file = './tests/dataloader/dummy_gpt2vocab/vocab.json'
 		merges_file = './tests/dataloader/dummy_gpt2vocab/merges.txt'
+		from transformers.tokenization_gpt2 import GPT2Tokenizer
 		return PretrainedTokenizer(GPT2Tokenizer(vocab_file, merges_file, unk_token=unk_token))
 	return _load_PretrainedTokenizer
 
 
-class TestPretrainedTokenizer(TestTokernizer):
-	
+class TestPretrainedTokenizer(TestTokenizer):
+
 	@pytest.mark.dependency()
 	def test_init(self, load_PretrainedTokenizer):
 		sentences = [
@@ -110,7 +108,7 @@ class TestPretrainedTokenizer(TestTokernizer):
 		]
 		super().base_test_tokenize_sentences(load_PretrainedTokenizer(), sentences)
 		super().base_test_tokenize_sessions(load_PretrainedTokenizer(), sessions)
-	
+
 	@pytest.mark.dependency(depends=["TestPretrainedTokenizer::test_init"])
 	def test_get_setting_hash(self, load_PretrainedTokenizer):
 		toker1 = load_PretrainedTokenizer(unk_token='<|endoftext|>')
